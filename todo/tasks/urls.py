84085e39a8b49b96c0d2573bc3a304f29c9051a5
luizeduardosb <luizeduardo00736@gmail.com>
2023-08-31 15:55:02 -0300

    feat: initial commit projeto-todo

---
+++ todo/tasks/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('helloword/', views.helloword),
+    path('', views.taskList, name='task-list'),
+    path('task/<int:id>', views.taskView, name='task-view'),
+    path('newtask/', views.newTask, name='new-task'),
+    path('edit/<int:id>', views.editTask, name='edit-task'),
+    path('changestatus/<int:id>', views.changeStatus, name='change-status'),
+    path('dash/', views.dashboard, name='dashboard'),
+    path('delete/<int:id>', views.deleteTask, name='delete-task'),
+    path('yourname/<str:name>', views.yourName, name='your-name'),
+
+]
---
+++ todo/tasks/views.py
@@ -0,0 +1,101 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import HttpResponse
+from .forms import TaskForm
+from django.contrib import messages
+import datetime
+
+from .models import Task
+
+@login_required
+def taskList(request):
+    search = request.GET.get('search')
+
+    if search:
+        tasks = Task.objects.filter(title__icontains=search, user=request.user)
+
+    else:
+        tasks_list = Task.objects.all().order_by('-created_at').filter(user=request.user)
+
+        paginator = Paginator(tasks_list, 3)
+        page = request.GET.get('page')
+        tasks = paginator.get_page(page)
+
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+@login_required
+def taskView(request, id):
+    task =  get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+@login_required
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.user = request.user
+            task.save()
+            messages.info(request, 'Tarefa criada com sucesso!')
+            return redirect('/')
+
+    else:
+        form = TaskForm()
+        return render(request, 'tasks/addtask.html', {'form': form})
+
+@login_required
+def editTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form =  TaskForm(instance=task)
+
+    if(request.method == 'POST'):
+        form = TaskForm(request.POST, instance=task)
+
+        if(form.is_valid()):
+            task.save()
+            messages.info(request, 'Tarefa editada com sucesso!')
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+    
+    else:
+        return render(request, 'tasks/edittask.html', {'form': form, 'task': task})
+
+@login_required
+def deleteTask(request, id):
+    task = get_object_or_404(Task, pk=id)
+    task.delete()
+    messages.info(request, 'Tarefa deletada com sucesso!')
+    return redirect('/')
+
+@login_required
+def changeStatus(request, id):
+    task = get_object_or_404(Task, pk=id)
+
+    if(task.done == 'doing'):
+        task.done = 'done'
+    else:
+        task.done = 'doing'
+
+    task.save()
+
+    return redirect('/')
+
+@login_required
+def dashboard(request):
+    tasksDoneRecently = Task.objects.filter(done='done', update_at__gt=datetime.datetime.now()-datetime.timedelta(days=30), user=request.user).count()
+    tasksDone = Task.objects.filter(done='done', user=request.user).count()
+    tasksDoing = Task.objects.filter(done='doing', user=request.user).count()
+
+    return render(request, 'tasks/dash.html', 
+                  {'tasksrecently': tasksDoneRecently, 'tasksdone': tasksDone, 'tasksdoing': tasksDoing})
+
+
+def helloword(request):
+    return HttpResponse('Hello Word!')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
